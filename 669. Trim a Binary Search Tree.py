79f27d9f43467887b1782bcdc602bfacb49560d6
Jared Chen <471238650@qq.com>
2017-12-26 19:13:10 +0800

    669. Trim a Binary Search Tree

---
+++ 669. Trim a Binary Search Tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def trimBST(self, root, L, R):
+        if root == None:
+            return None
+        if root.val > R:
+            return self.trimBST(root.left, L, R)
+        elif root.val < L:
+            return self.trimBST(root.right, L, R)
+        else:
+            root.left = self.trimBST(root.left, L, R)
+            root.right = self.trimBST(root.right, L, R)
+        return root
