d923a94b53cd4ce2dcddba6979273d24dbc940a0
Jared Chen <471238650@qq.com>
2018-01-21 00:10:43 +0800

    time limit exceeded

---
+++ 26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 2:
+            return len(nums)
+        else:
+            dup_num = 0
+            total_len = len(nums)
+            i = 1
+            while(i < len(nums) and len(nums) > 1):
+                dup = False
+                while(dup == False and i < len(nums) and len(nums) > 1):
+                    j = i-1
+                    if nums[j] == nums[i]:
+                        dup = True
+                        dup_num += 1
+                        nums.remove(nums[j])
+                        i = 1
+                    else:
+                        i += 1
+            return total_len - dup_num
+
+solution = Solution()
+res = solution.removeDuplicates([1,1,1,1,2,2,2,3,3,4,5])
+print(res)
